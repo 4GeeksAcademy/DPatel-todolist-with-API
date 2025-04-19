9975edc891f676879cb360a2b1b52f5e3f1faaff
22devkpatel <22devkpatel@gmail.com>
2025-04-19 23:19:30 +0000

    delete tasks

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const API_URL = "https://playground.4geeks.com/todo/users/dpatel";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+	const [tasks, setTasks] = useState([]);
+	const [input, setInput] = useState("");
+
+
+	useEffect(() => {
+		fetchTasks();
+	}, []);
+
+	const fetchTasks = () => {
+		fetch(API_URL)
+			.then((res) => res.json())
+			.then((data) => {
+				if (Array.isArray(data.todos)) {
+					setTasks(data.todos);
+				}
+			})	
+			.catch((err) => console.error("Failed to load tasks:", err));
+	};
+
+
+	const addTask = (e) => {
+		if (e.key === "Enter" && input.trim()) {
+			const newTask = { label: input, is_done: false };
+
+			fetch(`https://playground.4geeks.com/todo/todos/dpatel` ,{
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(newTask),
+			})
+				.then((res) => res.json())
+				.then(() => {
+					setInput("");
+					fetchTasks();
+				})
+				.catch((err) => console.error("Failed to add task:", err));
+		}
+	};
+
+
+	const deleteTask = (taskId) => {
+		fetch(`https://playground.4geeks.com/todo/todos/${taskId}`, {
+			method: "DELETE",
+		})
+			.then((res) => {
+				if (res.ok) fetchTasks();
+			})
+			.catch((err) => console.error("Failed to delete task:", err));
+	};  
+
+}
 
 export default Home;
